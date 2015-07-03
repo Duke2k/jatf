16537a23f61178cba445ede38e0deacc6bf9cf38
Samuel Eickelberg <samuel.eickelberg@mybet.com>
2015-07-03 08:50:42 +0200

    Updated ArchitectureTestConstants to use default values when properties cannot be evaluated.

--- src/main/java/jatf/common/ArchitectureTestConstants.java
+++ src/main/java/jatf/common/ArchitectureTestConstants.java
@@ -25,6 +25,7 @@ import static jatf.common.ArchitectureTestRunListener.report;
 public class ArchitectureTestConstants {
 
     private static Properties properties;
+
     static {
         properties = new Properties();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -35,22 +36,38 @@ public class ArchitectureTestConstants {
             report("Could not load jatf.properties:", e);
         }
     }
-    public static final String[] SCOPES = properties.getProperty("scopes").split(",");
+    public static String[] SCOPES = "".split(",");
+    public static String ROOT_FOLDER = ".";
+    public static double INSTABILITY_STRICT = 0.4;
+    public static double INSTABILITY_LOOSE = 0.6;
+    public static int MAXIMUM_CCN = 10;
+    public static double MAX_HALSTEAD_DELIVERED_BUGS = 3;
+    public static int MAX_NUMBER_OF_METHODS_PER_CLASS = 20;
+    public static int MAX_NUMBER_OF_STATEMENTS_PER_METHOD = 20;
+    public static int MAX_DEPTH_FOR_DFS = 10;
+    public static double MIN_DEGREE_OF_PURITY = 0.5;
+    public static int MAX_CHAINED_METHOD_CALLS = 5;
 
-    public static final String ROOT_FOLDER = properties.getProperty("root.folder");
-
-    public static final double INSTABILITY_STRICT = Double.parseDouble(properties.getProperty("instability.strict"));
-    public static final double INSTABILITY_LOOSE = Double.parseDouble(properties.getProperty("instability.loose"));
-    public static final int MAXIMUM_CCN = Integer.parseInt(properties.getProperty("cyclomatic-complexity.maximum"));
-    public static final double MAX_HALSTEAD_DELIVERED_BUGS =
-            Double.parseDouble(properties.getProperty("halstead-delivered-bugs.maximum"));
-    public static final int MAX_NUMBER_OF_METHODS_PER_CLASS =
-            Integer.parseInt(properties.getProperty("methods-per-class.maximum"));
-    public static final int MAX_NUMBER_OF_STATEMENTS_PER_METHOD =
-            Integer.parseInt(properties.getProperty("statements-per-method.maximum"));
-    public static final int MAX_DEPTH_FOR_DFS = Integer.parseInt(properties.getProperty("depth-for-dfs.maximum"));
-    public static final double MIN_DEGREE_OF_PURITY =
-            Double.parseDouble(properties.getProperty("degree-of-purity.minimum"));
-    public static final int MAX_CHAINED_METHOD_CALLS =
-            Integer.parseInt(properties.getProperty("chained-method-calls.maximum"));
+    static {
+        try {
+            SCOPES = properties.getProperty("scopes").split(",");
+            ROOT_FOLDER = properties.getProperty("root.folder");
+            INSTABILITY_STRICT = Double.parseDouble(properties.getProperty("instability.strict"));
+            INSTABILITY_LOOSE = Double.parseDouble(properties.getProperty("instability.loose"));
+            MAXIMUM_CCN = Integer.parseInt(properties.getProperty("cyclomatic-complexity.maximum"));
+            MAX_HALSTEAD_DELIVERED_BUGS =
+                    Double.parseDouble(properties.getProperty("halstead-delivered-bugs.maximum"));
+            MAX_NUMBER_OF_METHODS_PER_CLASS =
+                    Integer.parseInt(properties.getProperty("methods-per-class.maximum"));
+            MAX_NUMBER_OF_STATEMENTS_PER_METHOD =
+                    Integer.parseInt(properties.getProperty("statements-per-method.maximum"));
+            MAX_DEPTH_FOR_DFS = Integer.parseInt(properties.getProperty("depth-for-dfs.maximum"));
+            MIN_DEGREE_OF_PURITY =
+                    Double.parseDouble(properties.getProperty("degree-of-purity.minimum"));
+            MAX_CHAINED_METHOD_CALLS =
+                    Integer.parseInt(properties.getProperty("chained-method-calls.maximum"));
+        } catch (Exception e) {
+            ArchitectureTestRunListener.report("Unable to query properties, using default constraints instead.", e);
+        }
+    }
 }
